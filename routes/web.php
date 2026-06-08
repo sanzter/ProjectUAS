9e5293c2e010b1fcbd8cb4ebddeef6575bc8de51
sanzter <peterkingjo38@gmail.com>
2026-06-08 17:06:16 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
+    return view('welcome');
     return redirect()->route('dashboard');
 });
 
